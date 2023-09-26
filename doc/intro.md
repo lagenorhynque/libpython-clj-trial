fdbad021146d232481eaa3553feab65227517a35
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2023-09-26 09:10:21 +0900

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to lagenorhynque/libpython-clj-trial
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/lagenorhynque/libpython_clj_trial_test.clj
@@ -0,0 +1,7 @@
+(ns lagenorhynque.libpython-clj-trial-test
+  (:require [clojure.test :refer :all]
+            [lagenorhynque.libpython-clj-trial :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
