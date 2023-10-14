91b3805f6dc3c7f81d3122a089552fa1462579e8
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2023-10-15 06:46:29 +0900

    Add libpython-clj minimal examples

--- src/lagenorhynque/libpython_clj_trial.clj
+++ src/lagenorhynque/libpython_clj_trial.clj
@@ -1,5 +1,8 @@
 (ns lagenorhynque.libpython-clj-trial
-  (:gen-class))
+  (:gen-class)
+  (:require
+   [libpython-clj2.python :as py :refer [py. py.. py.-]]
+   [libpython-clj2.require :refer [require-python]]))
 
 (defn greet
   "Callable entry point to the application."
@@ -10,3 +13,19 @@
   "I don't do a whole lot ... yet."
   [& args]
   (greet {:name (first args)}))
+
+(comment
+  (require-python
+   '[numpy :as np]
+   '[numpy.random :as np.random]
+   '[pandas :as pd])
+
+  (def dates (pd/date_range "1/1/2000" :periods 8))
+  (def table (pd/DataFrame
+              (np.random/randn 8 4)
+              :index dates
+              :columns ["A" "B" "C" "D"]))
+  (def row-date (pd/date_range
+                 :start "2000-01-01"
+                 :end "2000-01-01"))
+  (py/get-item (py.- table :loc) row-date))
